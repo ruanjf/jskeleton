25dcdf963fe4c92f4295599f06eb5d2a8a4a246b
rjf <runjf@qq.com>
2013-09-13 14:08:18 +0800

    添加包说明

---
+++ jskeleton-core/src/main/java/com/rongji/jskeleton/core/eventListen/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 事件支持
+ * 
+ * 观察者模式
+ * 
+ * @author rjf
+ *
+ */
+package com.rongji.jskeleton.core.eventListen;
---
+++ jskeleton-core/src/main/java/com/rongji/jskeleton/core/tools/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 一些工具类
+ * 
+ * @author rjf
+ *
+ */
+package com.rongji.jskeleton.core.tools;
